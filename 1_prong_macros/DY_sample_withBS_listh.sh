67dc3e22ba596ec378a87cf94ab1751485421161
vmuralee <31473363+vmuralee@users.noreply.github.com>
2019-04-30 19:26:54 +0530

    script file added

---
+++ 1_prong_macros/DY_sample_withBS_listh.sh
@@ -0,0 +1,7 @@
+#!/bin/bash                                                                                                                                           
+
+MCdir_SUSY=/nfs/dust/cms/user/klundert/HiggsCPNTuples/2017/MC/
+
+
+
+ls $MCdir_SUSY/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/*.root > DYToTauTau_with
